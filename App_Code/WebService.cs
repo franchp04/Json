7649f2bc91549de99bddb7a94caa18abd0028f89
Diego Hinojosa <dfhinojosa@udlanet.ec>
2014-12-16 20:46:12 -0500

    subir

---
+++ App_Code/WebService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Script.Serialization;
+
+/// <summary>
+/// Descripción breve de WebService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// Para permitir que se llame a este servicio Web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+[System.Web.Script.Services.ScriptService]
+public class WebService : System.Web.Services.WebService {
+
+    public WebService () {
+
+        //Eliminar la marca de comentario de la línea siguiente si utiliza los componentes diseñados 
+        //InitializeComponent(); 
+    }
+
+    DataClassesDataContext dc = new DataClassesDataContext();
+
+    [WebMethod]
+    public string HelloWorld() {
+        return "Hola a todos";
+    }
+
+    [WebMethod]
+    public string GetContat(string id)
+    {
+        var json = "";
+        var contact = from result in dc.automovil
+                      where result.codigo == Int32.Parse(id)
+                      select result;
+        JavaScriptSerializer jss = new JavaScriptSerializer();
+        json = jss.Serialize(contact);
+        return json;
+    }
+    
+}
